c62d48f1cfa1d65b202a3eb3b5f46ba61d720a8b
kongnyc <bbsyoow@gmail.com>
2020-03-21 11:14:24 -0400

    makeList for my last name

--- 01_node/makeList.js
+++ 01_node/makeList.js
@@ -4,9 +4,16 @@ const { Node } = require('./node');
  * Create a linked list with three nodes, A -> B -> C
  * There are many ways you can do this!
  */
-const list = new Node('A');
+const list = new Node('Y');
+const list2 = new Node('A');
+const list3 = new Node('N');
+const list4 = new Node('G');
+list.next=list2
+list2.next=list3
+list3.next=list4
 // your code here
 
+console.log(list)
 console.log(JSON.stringify(list));
 
 /**
