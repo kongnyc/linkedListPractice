4e1c2f3cea528bdbee88e7894e19c1a14459340a
Ohidurr <Ohidurrahman96@gmail.com>
2020-03-21 11:27:33 -0400

    made linklist for food

--- 01_node/fromScratch.js
+++ 01_node/fromScratch.js
@@ -6,3 +6,22 @@
  * DO NOT import the Node class from ./node.js
  */
 
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+}
+
+const favoriteFood = ("rice")
+const letter = new Node("R")    
+const letter2 = new Node("I")
+const letter3 = new Node("C")
+const letter4 = new Node("E")
+
+
+letter.next = letter2
+letter2.next = letter3
+letter3.next = letter4 
+letter4.next = null
+console.log(letter)
